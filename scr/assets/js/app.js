ddebfb26c3e9ad19acf1f958e5eb029a4a8d38ec
Alessandro Fernandes Antunes <alesfernandes123@gmail.com>
2025-05-17 17:03:22 +0000

    criado o js e retirando o css do arquivo porque tava dando bugue alem de renovar o bootstrap como novos comandos

--- scr/assets/js/app.js
+++ scr/assets/js/app.js
@@ -1,4 +1,20 @@
-function main () {
+    function Main() {
+      const button = document.getElementById("button");
+      button.addEventListener("click", function() {
+        // lê o valor selecionado
+        var material = document.getElementById("material").value;
 
-    
-}
+        console.log("Material selecionado:", material);
+        // se quiser usar como número:
+        var matNum = parseInt(material, 10);
+        console.log("Como número:", matNum);
+      });
+    }
+
+
+/*     <option value="0">Escolha seu material</option>
+    <option value="1">Plastico</option>
+    <option value="2">Metal</option>
+    <option value="3">Papel</option>
+    <option value="3">Vidro</option>
+    <option value="3">Material Organico</option> */
