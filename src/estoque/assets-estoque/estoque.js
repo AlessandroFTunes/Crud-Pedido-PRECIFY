4af713767aed58bf55007a9558baa8d76100a9f8
Alessandro Fernandes Antunes <alesfernandes123@gmail.com>
2025-05-24 13:46:00 +0000

    atualizando json-server

--- src/estoque/assets-estoque/estoque.js
+++ src/estoque/assets-estoque/estoque.js
@@ -1,72 +1,85 @@
 // colocar e atualizar a pagina
 function atualizar() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json = JSON.parse(xhttp.responseText);
-            var qnd = json.length;
-
-            var html = "";
-            for (let x = 0; x < qnd; x++) {
-                html += "<li class='list-group-item'>" +
-                        "<a href='detalhes.html?id=" + json[x].id + "'>" +
-                        json[x].id + " - " + json[x].titulo +
-                        "</a></li>";
+    fetch("http://localhost:3000/materiais")
+        .then(response => response.json())
+        .then(json => {
+            let html = "";
+            for (let material of json) {
+                html += "<tr>";
+                html += "<th scope='row' class='text-center' id='id-" + material.id + "'>" + material.id + "</th>";
+                html += "<td id='tipo-" + material.id + "'>" + material.tipo + "</td>";
+                html += "<td id='preco-" + material.id + "'>R$ " + material.preco.toFixed(2) + "</td>";
+                html += "<td id='peso-" + material.id + "'>" + material.peso + " kg</td>";
+                html += "<td id='localidade-" + material.id + "'>" + material.localidade + "</td>";
+                html += "<td class='text-center'>";
+                html += "<button class='btn btn-sm btn-outline-primary me-1' id='editar-" + material.id + "'><i class='bi bi-pencil'></i></button>";
+                html += "<button class='btn btn-sm btn-outline-danger' id='excluir-" + material.id + "'><i class='bi bi-trash'></i></button>";
+                html += "</td>";
+                html += "</tr>";
             }
-
             document.getElementById("livros_db").innerHTML = html;
-        }
-    };
-    xhttp.open("GET", "http://localhost:3000/livros", true);
-    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhttp.send();
+        })
+        .catch(error => console.error('Erro ao buscar materiais:', error));
 }
+
 
 document.addEventListener("DOMContentLoaded", function () {
     atualizar();
 
-    const button = document.getElementById("btnGravar");
+    const button = document.getElementById("btnClicar");
     button.addEventListener("click", function () {
-        let livros = {
+        let materiais = {
             "id": document.getElementById("id").value,
-            "titulo": document.getElementById("titulo").value,
-            "autor": document.getElementById("autor").value,
-            "ano": document.getElementById("ano").value
+            "tipo": document.getElementById("tipo").value,
+            "preco": parseFloat(document.getElementById("preco").value),
+            "peso": parseFloat(document.getElementById("peso").value),
+            "localidade": document.getElementById("localidade").value
         };
 
-        var xhttpPost = new XMLHttpRequest();
-        xhttpPost.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 201) {
+        fetch("http://localhost:3000/materiais", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(materiais)
+        })
+        .then(response => {
+            if (response.ok) {
                 atualizar(); // Atualiza a lista após salvar
+            } else {
+                console.error('Erro ao salvar material');
             }
-        };
-        xhttpPost.open("POST", "http://localhost:3000/livros", true);
-        xhttpPost.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        xhttpPost.send(JSON.stringify(livros));
+        })
+        .catch(error => console.error('Erro na requisição:', error));
     });
 });
 
-// pegar os elementos
+
+// Pegar os elementos da URL
 var parametros = new URLSearchParams(location.search);
 var id = parametros.get("id");
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var livro = JSON.parse(xhttp.responseText);
-
-        if (livro) {
-            document.getElementById("titulo").innerHTML = livro.titulo;
-            document.getElementById("autor").innerHTML = livro.autor;
-            document.getElementById("ano").innerHTML = livro.ano;
+if (id) {
+    fetch("http://localhost:3000/materiais/" + id, {
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    })
+    .then(response => response.json())
+    .then(material => {
+        if (material) {
+            document.getElementById("tipo").innerHTML = material.tipo;
+            document.getElementById("preco").innerHTML = "R$ " + material.preco.toFixed(2);
+            document.getElementById("peso").innerHTML = material.peso + " kg";
+            document.getElementById("localidade").innerHTML = material.localidade;
             document.getElementById("imagem").innerHTML = "<img src='assets/img/default.png'>";
         } else {
-            document.getElementById("titulo").innerHTML = "Não Existe";
-            document.getElementById("autor").innerHTML = "Não Existe";
-            document.getElementById("ano").innerHTML = "Não Existe";
+            document.getElementById("tipo").innerHTML = "Não Existe";
+            document.getElementById("preco").innerHTML = "Não Existe";
+            document.getElementById("peso").innerHTML = "Não Existe";
+            document.getElementById("localidade").innerHTML = "Não Existe";
         }
-    }
-};
-xhttp.open("GET", "http://localhost:3000/livros/" + id, true);
-xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-xhttp.send();
+    })
+    .catch(error => console.error('Erro ao buscar material:', error));
+}
+
